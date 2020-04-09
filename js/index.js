d74be9e59c2f5abd21516b857483565b7afcd2b1
unknown <uurfgdh@gmail.com>
2020-04-09 12:02:22 +0300

    name

--- js/index.js
+++ js/index.js
@@ -1,12 +1,8 @@
 $(window).scroll(function(){
             if ($(window).scrollTop() > '200'){
                     $('.navbar').css({"background":"black"});
-//                    $('.navbar').css({"opacity":"0.7"});
+                    $('.navbar').css({"opacity":"0.8"});
                 }
-            else if(window.innerWidth <= 768){
-                $('.navbar').css({"background":"black"});
-                $('.navbar').css({"top":"20px"});
-            }
             else {
                 $('.navbar').css({"background":"transparent"});
             }
